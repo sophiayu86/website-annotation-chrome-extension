0abdc4a2f1554201955f1c6c1d9efcabf6c80416
sophiayu86 <sophiayu86@gmail.com>
2024-03-18 21:49:52 +0800

    feat: select from popup and print in console

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [note, setNote] = useState<string>("");
+  const [selObj, setSelObj] = useState<Selection | null>(null);
+ 
+  const onclick = async() => {
+    const selTxt = getSelectionTxt();
+    let [tab] = await chrome.tabs.query({ active: true });
+    chrome.scripting.executeScript<(string | null)[], void>({
+        target: { tabId: tab.id! },
+        args: [note, selTxt],
+        func: (note,selTxt) => {
+            document.body.style.backgroundColor = note || "white";
+            console.log("Sel:", selTxt);
+        }
+    });
+  }
+  const handleSelectionChange = () => {
+    setSelObj(window.getSelection());
+  };
+  const getSelectionTxt = () => {
+    return selObj ? selObj.toString() : null;
+  };
+  // const handleClick =() => {
+  //   console.log("Select:",getSelectionTxt());
+  // }
+
+  useEffect(() => {
+    document.addEventListener("selectionchange", handleSelectionChange);
+    
+    return () => {
+      document.removeEventListener("selectionchange", handleSelectionChange);
+    };
+  }, []); 
 
   return (
     <>
@@ -18,9 +49,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <button onClick={onclick}>
         </button>
+        {/* <button onClick={handleClick}>
+        </button> */}
+        <input type="text" onChange={(e)=> setNote(e.target.value)} value={note}/>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
